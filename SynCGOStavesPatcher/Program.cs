2862ea193699ac9b5ab79a6342f606f3b2a08f86
Minizbot2012 <mini@zbots.org>
2020-11-16 16:59:44 -0500

    Initial Commit

---
+++ SynCGOStavesPatcher/Program.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Linq;
+using Mutagen.Bethesda;
+using Mutagen.Bethesda.Synthesis;
+using Mutagen.Bethesda.Skyrim;
+using Mutagen.Bethesda.FormKeys.SkyrimSE;
+
+namespace SynPatcher
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            return SynthesisPipeline.Instance.Patch<ISkyrimMod, ISkyrimModGetter>(
+                args: args,
+                patcher: RunPatch,
+                userPreferences: new UserPreferences()
+                {
+                    ActionsForEmptyArgs = new RunDefaultPatcher()
+                    {
+                        IdentifyingModKey = "SynCGOStaves.esp",
+                        TargetRelease = GameRelease.SkyrimSE,
+                        BlockAutomaticExit = true,
+                    }
+                });
+        }
+
+        public static void RunPatch(SynthesisState<ISkyrimMod, ISkyrimModGetter> state)
+        {
+            var mk = ModKey.FromNameAndExtension("DSerCombatGameplayOverhaul.esp");
+            if(state.LoadOrder.ContainsKey(mk)) {
+                foreach(var weap in state.LoadOrder.PriorityOrder.OnlyEnabled().Weapon().WinningOverrides()) {
+                    if(weap.Keywords?.Contains(Skyrim.Keyword.WeapTypeStaff)??false) {
+                        if(weap.FormKey.ModKey.FileName.Contains("CGOStaves")) {
+                            var newweap = state.PatchMod.Weapons.GetOrAddAsOverride(weap);
+                            newweap.BlockBashImpact = Skyrim.ImpactDataSet.WPNBashBowImpactSet;
+                            newweap.AlternateBlockMaterial = Skyrim.MaterialType.MaterialBlockBowsStaves;
+                            newweap.ImpactDataSet = Skyrim.ImpactDataSet.WPNzBluntImpactSet;
+                            newweap.AttackFailSound = Skyrim.SoundDescriptor.WPNSwing2Hand;
+                        }
+                    }
+                }
+            } else {
+                Console.WriteLine("DSerCombatGameplayOverhaul.esp not found in load order, doing nothing");
+            }
+        }
+    }
+}
