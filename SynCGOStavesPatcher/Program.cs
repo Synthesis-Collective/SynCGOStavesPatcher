6401c00711b0a44f9ca296b67ca3091fba1b3a42
Minizbot2012 <mini@zbots.org>
2021-01-03 11:51:55 -0500

    Port checks to runability check

--- SynCGOStavesPatcher/Program.cs
+++ SynCGOStavesPatcher/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using System.Threading.Tasks;
 using Mutagen.Bethesda;
 using Mutagen.Bethesda.Synthesis;
 using Mutagen.Bethesda.Skyrim;
@@ -9,41 +10,34 @@ namespace SynPatcher
 {
     public class Program
     {
-        public static int Main(string[] args)
+        public static async Task<int> Main(string[] args)
         {
-            return SynthesisPipeline.Instance.Patch<ISkyrimMod, ISkyrimModGetter>(
-                args: args,
-                patcher: RunPatch,
-                userPreferences: new UserPreferences()
-                {
-                    ActionsForEmptyArgs = new RunDefaultPatcher()
-                    {
-                        IdentifyingModKey = "SynCGOStaves.esp",
-                        TargetRelease = GameRelease.SkyrimSE,
-                        BlockAutomaticExit = true,
-                    }
-                });
+            return await SynthesisPipeline.Instance.AddRunnabilityCheck(Runable).AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch).Run(args, new RunPreferences() {
+               ActionsForEmptyArgs = new RunDefaultPatcher()
+               {
+                   IdentifyingModKey = "SynCGOStaves.esp",
+                   TargetRelease = GameRelease.SkyrimSE
+               } 
+            });
         }
 
-        public static void RunPatch(SynthesisState<ISkyrimMod, ISkyrimModGetter> state)
+        public static void RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
         {
-            var mk = ModKey.FromNameAndExtension("DSerCombatGameplayOverhaul.esp");
-            if(state.LoadOrder.ContainsKey(mk)) {
-                foreach(var weap in state.LoadOrder.PriorityOrder.OnlyEnabled().Weapon().WinningOverrides()) {
-                    if(weap.Keywords?.Contains(Skyrim.Keyword.WeapTypeStaff)??false) {
-                        if(!weap.FormKey.ModKey.FileName.Contains("CGOStaves")) {
-                            Console.WriteLine($"Patching staff {weap.Name}");
-                            var newweap = state.PatchMod.Weapons.GetOrAddAsOverride(weap);
-                            newweap.BlockBashImpact = Skyrim.ImpactDataSet.WPNBashBowImpactSet;
-                            newweap.AlternateBlockMaterial = Skyrim.MaterialType.MaterialBlockBowsStaves;
-                            newweap.ImpactDataSet = Skyrim.ImpactDataSet.WPNzBluntImpactSet;
-                            newweap.AttackFailSound = Skyrim.SoundDescriptor.WPNSwing2Hand;
-                        }
+            foreach(var weap in state.LoadOrder.PriorityOrder.OnlyEnabled().Weapon().WinningOverrides()) {
+                if(weap.Keywords?.Contains(Skyrim.Keyword.WeapTypeStaff)??false) {
+                    if(!weap.FormKey.ModKey.FileName.Contains("CGOStaves")) {
+                        Console.WriteLine($"Patching staff {weap.Name}");
+                        var newweap = state.PatchMod.Weapons.GetOrAddAsOverride(weap);
+                        newweap.BlockBashImpact = Skyrim.ImpactDataSet.WPNBashBowImpactSet;
+                        newweap.AlternateBlockMaterial = Skyrim.MaterialType.MaterialBlockBowsStaves;
+                        newweap.ImpactDataSet = Skyrim.ImpactDataSet.WPNzBluntImpactSet;
+                        newweap.AttackFailSound = Skyrim.SoundDescriptor.WPNSwing2Hand;
                     }
                 }
-            } else {
-                Console.WriteLine("DSerCombatGameplayOverhaul.esp not found in load order, doing nothing");
             }
+        }
+        public static async Task Runable(IRunnabilityState state) {
+            state.LoadOrder.AssertHasMod(ModKey.FromNameAndExtension("DSerCombatGameplayOverhaul.esp"));
         }
     }
 }
