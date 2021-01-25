937f065f2e034f473f24e5f244a2d03b321a3e18
Minizbot2012 <mini@zbots.org>
2021-01-25 17:41:57 -0500

    Clean Code, muta/syn update

--- SynCGOStaves/Program.cs
+++ SynCGOStaves/Program.cs
@@ -8,26 +8,30 @@ using Mutagen.Bethesda.FormKeys.SkyrimSE;
 
 namespace SynCGOStaves
 {
-    public class Program
+    internal class Program
     {
         public static async Task<int> Main(string[] args)
         {
-            return await SynthesisPipeline.Instance.AddRunnabilityCheck(Runable).AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch).Run(args, new RunPreferences() {
-               ActionsForEmptyArgs = new RunDefaultPatcher()
-               {
-                   IdentifyingModKey = "SynCGOStaves.esp",
-                   TargetRelease = GameRelease.SkyrimSE
-               } 
+            return await SynthesisPipeline.Instance.AddRunnabilityCheck(Runable).AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch).Run(args, new RunPreferences()
+            {
+                ActionsForEmptyArgs = new RunDefaultPatcher()
+                {
+                    IdentifyingModKey = "SynCGOStaves.esp",
+                    TargetRelease = GameRelease.SkyrimSE
+                }
             });
         }
 
         public static void RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
         {
-            foreach(var weap in state.LoadOrder.PriorityOrder.OnlyEnabled().Weapon().WinningOverrides()) {
-                if(weap.Keywords?.Contains(Skyrim.Keyword.WeapTypeStaff)??false) {
-                    if(!weap.FormKey.ModKey.FileName.Contains("CGOStaves")) {
+            foreach (var weap in state.LoadOrder.PriorityOrder.OnlyEnabled().Weapon().WinningOverrides())
+            {
+                if (weap.Keywords?.Contains(Skyrim.Keyword.WeapTypeStaff) ?? false)
+                {
+                    if (!weap.FormKey.ModKey.FileName.Contains("CGOStaves"))
+                    {
                         Console.WriteLine($"Patching staff {weap.Name}");
-                        var newweap = state.PatchMod.Weapons.GetOrAddAsOverride(weap);
+                        Weapon? newweap = state.PatchMod.Weapons.GetOrAddAsOverride(weap);
                         newweap.BlockBashImpact = Skyrim.ImpactDataSet.WPNBashBowImpactSet;
                         newweap.AlternateBlockMaterial = Skyrim.MaterialType.MaterialBlockBowsStaves;
                         newweap.ImpactDataSet = Skyrim.ImpactDataSet.WPNzBluntImpactSet;
@@ -36,8 +40,10 @@ namespace SynCGOStaves
                 }
             }
         }
-        public static async Task Runable(IRunnabilityState state) {
+        public static Task Runable(IRunnabilityState state)
+        {
             state.LoadOrder.AssertHasMod(ModKey.FromNameAndExtension("DSerCombatGameplayOverhaul.esp"));
+            return Task.CompletedTask;
         }
     }
 }
